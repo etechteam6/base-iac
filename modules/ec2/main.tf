d754d0b3cc5dffd57644fe15efb5a970fb8fcffe
KingMoses33 <136907140+KingMoses33@users.noreply.github.com>
2024-03-01 00:45:00 -0500

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.38.0"
+    }
+  }
+}
 
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+module "ec2" {
+    source = "../modules/ec2"
+    num = 10
+    region = "us-east-1"        #   ami must match the region too because amis are region specific 
+  
+}
